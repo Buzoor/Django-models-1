74e3c20801ebb61c7b7b0c9eb4250fb131377c73
Marvelous Obiji <marvelous@gmail.com>
2022-06-19 05:15:05 +0100

    updated

--- marvelous_obiji/blog/models.py
+++ marvelous_obiji/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 # Create your models here.
-class blog(models.Model):
+class Post(models.Model):
     Tittle = models.CharField(max_length=200)
     Text = models.TextField()
     Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
